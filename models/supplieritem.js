0cced092ad32749bd93517664e308be9e2a67f00
Dimitri <deathmitri@gmail.com>
2017-10-12 14:33:03 +0700

    Added formatUang helper

--- models/supplieritem.js
+++ models/supplieritem.js
@@ -1,9 +1,15 @@
 'use strict';
+const formatUang = require('../helpers/formatUang');
 module.exports = (sequelize, DataTypes) => {
   var SupplierItem = sequelize.define('SupplierItem', {
     SupplierId: DataTypes.INTEGER,
     ItemId: DataTypes.INTEGER,
     price: DataTypes.INTEGER
   });
+
+  SupplierItem.prototype.getPrice = function () {
+    return formatUang(this.price);
+  }
+
   return SupplierItem;
 };
