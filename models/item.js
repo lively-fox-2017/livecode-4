1a164dff4e38726d5b428f3119c44792bf9df294
kholes <cholis.elbazar@gmail.com>
2017-10-12 16:47:35 +0700

    kedua

--- models/item.js
+++ models/item.js
@@ -3,7 +3,19 @@ module.exports = function(sequelize, DataTypes) {
   var Item = sequelize.define('Item', {
     name: DataTypes.STRING,
     brand: DataTypes.STRING,
-    codeitem: DataTypes.STRING
+    codeitem:DataTypes.STRING
+/*
+    , {
+      type:DataTypes.STRING,
+      validate:{
+        is: /(HP|SW|LP)\d{4}/,
+        len: { 
+           args: [2,7],
+           msg: "The password length should be between 7 and 42 characters."
+        }
+      }
+    }
+*/
   });
   Item.associate = (model) => {
     Item.hasMany(model.SupplierItem,{foreignKey:'ItemId'})
--- routs/suppliers.js
+++ routs/suppliers.js
@@ -3,31 +3,25 @@ const router = express.Router()
 const model = require('../models')
 router.get('/', (req,res) => {
 	model.Supplier.findAll().then(suppliers => {
-		let prom = []
-		suppliers.forEach(supp => {
-			prom.push(model.SupplierItem.findAll({where:{SupplierId:supp.id}}))
+		let prom = suppliers.map(supplier => {
+			return new Promise((resolve,reject) => {
+				supplier.getItems().then(items => {
+					if(items){
+						let newSupplier = items.map(item => {
+							return item.name
+						})
+						supplier["item_name"] = newSupplier
+					} 
+					else {
+						supplier["item_name"] = "No Item Yet"
+					}
+					resolve(supplier)
+				})
+			})
 		})
-
-		// let prom = suppliers.map(supplier => {
-		// 	return new Promise((resolve,reject) => {
-		// 		supplier.getSupplierItems().then(items => {
-		// 			if(items){
-		// 				let newSupplier = items.map(item => {
-		// 					supplier['item_name'] = item.name
-		// 				})
-		// 			} else {
-		// 				supplier['item_name'] = "No Item Yet"
-		// 			}
-		// 			console.log(items)
-		// 		})
-		// 		console.log(supplier)
-		// 		resolve(supplier)
-		// 	})
-		// })
 		Promise.all(prom).then(suppliers => {
 			res.render('supplier', {data:suppliers})
 		})
-		// console.log(prom)
 	})
 })
 router.get('/add', (req,res) => {
@@ -51,6 +45,16 @@ router.post('/edit/:id', (req,res) => {
 router.get('/delete/:id', (req,res) => {
 	model.Supplier.destroy({where:{id:req.params.id}}).then(del => {
 		res.redirect('/suppliers')
+	})
+})
+router.get('/assign/:id', (req,res) => {
+	model.Supplier.findOne({where:{id:req.params.id}}).then(row_supplier => {
+		model.Item.findAll().then(row_items => {
+			res.render('supplier_assign_item', {row_supplier:row_supplier,row_items:row_items})
+		})
+	})
+	.catch(err => {
+		res.send(err)
 	})
 })
 module.exports = router
