9bae449ce0ef4ef0782b954ef7e32d8b611b4dba
Nandira Paturohman <nandirasp@gmail.com>
2017-10-12 16:43:09 +0700

    commit save

--- routes/supplier.js
+++ routes/supplier.js
@@ -8,6 +8,7 @@ router.get('/', (req, res)=>{
       model: model.Item
     }]
   }).then(dataSupplier=>{
+    // console.log(dataSupplier[1].Items);
     // res.send(dataSupplier);
     res.render('supplier', {suppliers:dataSupplier})
   })
@@ -39,6 +40,29 @@ router.post('/edit/:id', (req, res)=>{
 
 router.get('/delete/:id', (req, res)=>{
   model.Supplier.destroy({where:req.params}).then(dataSupplier=>{
+    res.redirect('/suppliers')
+  })
+})
+
+router.get('/:id/additem', (req, res)=>{
+  Promise.all([
+  model.Supplier.findOne({
+    where: req.params,
+    include: [{
+      model: model.Item
+    }]
+  }),
+  model.Item.findAll()
+  ]).then(data=>{
+    // res.send(data[0]);
+    res.render('assign-item', {supplier:data[0], items:data[1]})
+  })
+})
+
+router.post('/:id/additem', (req, res)=>{
+  req.body.SupplierId = req.params.id
+  console.log(req.body);
+  model.SupplierItem.create(req.body).then(dataSuplier=>{
     res.redirect('/suppliers')
   })
 })
