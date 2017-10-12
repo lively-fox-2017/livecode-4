625ae810d57717395e00feb46ecb9a1773f3e728
azhariemuhammad <azharie_muhammad@live.com>
2017-10-12 15:53:08 +0700

    checkpoint2

--- routers/suppliers.js
+++ routers/suppliers.js
@@ -14,8 +14,30 @@ router.get('/',function(req, res){
 })
 
 router.get('/add', function(req, res){
-  //model.Supplier.create()
+  res.render('suppliers/addSuppliers')
 })
+
+router.post('/add', function(req, res){
+  //res.send('woooooS')
+  //console.log('hello');
+  model.Supplier.create({
+    name:req.body.name,
+    kota:req.body.kota
+  })
+  .then(dataSuppliers=>{
+    console.log(dataSuppliers);
+    res.redirect('/suppliers')
+  })
+})
+
+router.get('/edit/:id',function(req,res){
+  model.Supplier.findById({where:{id:req.params.id}})
+  .then(dataSuppliers=>{
+    console.log(dataSuppliers, 'foo');
+    res.render('/suppliers/edit', {dataSuppliers:dataSuppliers})
+  })
+})
+
 
 
 
